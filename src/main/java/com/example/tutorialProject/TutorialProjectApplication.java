db49f72b55d96bb40e2f7c6393e46beee279d58b
y.kawazoe <yuu1209dai@gmail.com>
2025-02-11 17:10:30 +0900

    チュートリアル作成

---
+++ src/main/java/com/example/tutorialProject/TutorialProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorialProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialProjectApplication.class, args);
+	}
+
+}
