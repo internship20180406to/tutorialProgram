0da75a3902a9c09413c8df624c7d4c63ab2a5d83
ffg-training <internship0216to0222@gmail.com>
2025-02-14 17:12:36 +0900

    Test

--- src/main/java/com/example/tutorialProject/service/User2Service.java
+++ src/main/java/com/example/tutorialProject/service/User2Service.java
@@ -7,9 +7,9 @@ import java.util.Objects;
 
 @Service
 public class User2Service {
-    String name = "";
-    String birthplace = "";
-    String school = "";
+    String name = "直江兼続";
+    String birthplace = "新潟県";
+    String school = "春日山城";
 
     public void user2(List<String> Users) {
         if (!Objects.equals(name, "")) {
