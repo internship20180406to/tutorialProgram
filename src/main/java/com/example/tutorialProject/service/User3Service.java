7f577f47ad06737a922fc11c036ffa235e969f36
ffg-training <internship0216to0222@gmail.com>
2025-08-22 18:56:49 +0900

    修正

--- src/main/java/com/example/tutorialProject/service/User3Service.java
+++ src/main/java/com/example/tutorialProject/service/User3Service.java
@@ -7,9 +7,9 @@ import java.util.Objects;
 
 @Service
 public class User3Service {
-    String name = "test";
-    String birthplace = "test";
-    String school = "test";
+    String name = "";
+    String birthplace = "";
+    String school = "";
 
     public void user3(List<String> Users) {
         if (!Objects.equals(name, "")) {
--- src/main/java/com/example/tutorialProject/service/User4Service.java
+++ src/main/java/com/example/tutorialProject/service/User4Service.java
@@ -7,9 +7,9 @@ import java.util.Objects;
 
 @Service
 public class User4Service {
-    String name = "test";
-    String birthplace = "test";
-    String school = "test";
+    String name = "";
+    String birthplace = "";
+    String school = "";
 
     public void user4(List<String> Users) {
         if (!Objects.equals(name, "")) {
